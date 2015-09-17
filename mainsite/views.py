721563b6e2015f42cdc7647a36dbc508ee4f7c68
Dan Madere <danmadere@gmail.com>
2015-09-16 22:57:22 -0400

    Fake order entry page so we have something to protect with authentication

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
 
-# Create your views here.
+class OrderFormView(TemplateView):
+    template_name = "mainsite/order_form.html"
