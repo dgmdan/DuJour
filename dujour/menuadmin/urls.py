d1046c0b28540f12ed36825b13978c42ef409497
Dan Madere <danmadere@gmail.com>
2015-10-04 17:07:15 -0400

    Improvements to menu admin skeleton, add leaflet JS library

--- dujour/menuadmin/urls.py
+++ dujour/menuadmin/urls.py
@@ -7,5 +7,5 @@ from dujour.menuadmin.views import MenuAdminIndexView, MenuAdminRestaurantView, 
 urlpatterns = [
     url(r'^$', login_required(MenuAdminIndexView.as_view()), name='menu_admin_index'),
     url(r'^restaurant/(?P<restaurant_id>[0-9]+)/$', login_required(MenuAdminRestaurantView.as_view()), name='menu_admin_restaurant'),
-    url(r'^menu/(?P<menu_id>[0-9]+)/$', login_required(MenuAdminMenuView.as_view()), name='menu_admin_menu'),
+    url(r'^menu/(?P<pk>[0-9]+)/$', login_required(MenuAdminMenuView.as_view()), name='menu_admin_menu'),
 ]
