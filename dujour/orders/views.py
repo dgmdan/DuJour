8f6bff645f12655e18334328706246c474b5f60b
Dan Madere <danmadere@gmail.com>
2015-10-09 16:07:48 -0400

    Work on graphical ordering

--- dujour/orders/views.py
+++ dujour/orders/views.py
@@ -3,15 +3,22 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, View
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import FormView
+import datetime
 
 from dujour.orders.forms import AddItemForm
+from dujour.restaurants.models import Restaurant, DayRestaurant, MenuItemRegion, Menu
 
 class GraphicalOrderEntryView(FormView):
     template_name = 'orders/graphical_entry.html'
     form_class = AddItemForm
 
-    def get(self, request, *args, **kwargs):
-        return super(GraphicalOrderEntryView, self).get(request, args, kwargs)
+    def get_context_data(self, **kwargs):
+        context = super(GraphicalOrderEntryView, self).get_context_data(**kwargs)
+        restaurant = DayRestaurant.objects.get(day_of_week=datetime.datetime.today().weekday()).restaurant
+        menu = Menu.objects.get(restaurant=restaurant)
+        context['object'] = menu
+        context['region_list'] = MenuItemRegion.objects.filter(menu_item__menu=menu)
+        return context
 
     def form_valid(self, form):
         pk = form.add_item()
--- dujour/restaurants/models.py
+++ dujour/restaurants/models.py
@@ -22,13 +22,13 @@ class DayRestaurant(models.Model):
         verbose_name_plural = 'Assigned days of week'
 
     DAY_CHOICES = (
-        (0, 'Sunday'),
-        (1, 'Monday'),
-        (2, 'Tuesday'),
-        (3, 'Wednesday'),
-        (4, 'Thursday'),
-        (5, 'Friday'),
-        (6, 'Saturday'),
+        (0, 'Monday'),
+        (1, 'Tuesday'),
+        (2, 'Wednesday'),
+        (3, 'Thursday'),
+        (4, 'Friday'),
+        (5, 'Saturday'),
+        (6, 'Sunday'),
     )
     restaurant = models.ForeignKey(Restaurant)
     day_of_week = models.IntegerField(choices=DAY_CHOICES)
