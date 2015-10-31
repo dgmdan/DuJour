d7dd3bf31e1f80b6e8d07dc22276b03dd7864e08
Dan Madere <danmadere@gmail.com>
2015-10-31 16:56:52 -0400

    Replace placeholder order page with links to 3 ordering methods

--- dujour/mainsite/views.py
+++ dujour/mainsite/views.py
@@ -2,3 +2,8 @@ from django.views.generic.base import TemplateView
 
 class OrderFormView(TemplateView):
     template_name = "mainsite/order_form.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(OrderFormView, self).get_context_data(**kwargs)
+        context['user_first_name'] = self.request.user.first_name
+        return context
